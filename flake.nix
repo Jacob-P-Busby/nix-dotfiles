4ba6cf7b2d26b70b51a57ae91c3f260cbff0479c
Jacob Busby <98309644+Jacob-P-Busby@users.noreply.github.com>
2024-03-22 14:27:32 +0800

    hiya

--- flake.nix
+++ flake.nix
@@ -4,16 +4,28 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-23.11";
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, ... }: 
+  outputs = { self, nixpkgs, home-manager, ... }: 
     let
       lib = nixpkgs.lib;
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+
     in {
     nixosConfigurations = {
       nixos = lib.nixosSystem {
-        system = "x86_64-linux";
-	modules = [ "configuration.nix" ];
+        inherit system;
+	modules = [ ./configuration.nix ];
+      };
+    };
+
+    homeConfigurations = {
+      jacob = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+	modules = [ ./home.nix ];
       };
     };
   };
