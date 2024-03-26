b6b73ecfe2204fa8483d433156fee00f7245db19
Jacob Busby <98309644+Jacob-P-Busby@users.noreply.github.com>
2024-03-26 14:17:00 +0800

    added bash aliases

--- home.nix
+++ home.nix
@@ -6,34 +6,11 @@
   home.username = "jacob";
   home.homeDirectory = "/home/jacob";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
   home.stateVersion = "23.11"; # Please read the comment before changing.
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
@@ -63,8 +40,18 @@
   #  /etc/profiles/per-user/jacob/etc/profile.d/hm-session-vars.sh
   #
   home.sessionVariables = {
-    # EDITOR = "emacs";
+    EDITOR = "nvim";
   };
+
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      ll = "ls -l";
+      la = "ls -a";
+      lls = "ls -la";
+      
+      kys = "shutdown now";
+    }
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
