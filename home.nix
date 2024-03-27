66ba0c41010edb4eefe05c004e9827f3fc6cd841
Jacob Busby <98309644+Jacob-P-Busby@users.noreply.github.com>
2024-03-27 11:32:20 +0800

    spotify

--- home.nix
+++ home.nix
@@ -11,6 +11,7 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
+    spotify
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
