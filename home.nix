69cedbfa312c7d51d0b47ecf4f642289f146560c
Jacob Busby <98309644+Jacob-P-Busby@users.noreply.github.com>
2024-04-11 23:07:41 +0800

    switched to logseq for FOSS

--- home.nix
+++ home.nix
@@ -10,8 +10,8 @@
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = with pkgs [
-    obsidian
+  home.packages = with pkgs; [
+    logseq
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
