0780d8c1cef0b98d0605be13e0de91d25643a0df
Jacob Busby <98309644+Jacob-P-Busby@users.noreply.github.com>
2024-04-12 18:30:09 +0800

    added hyprland config to home-manager

--- home.nix
+++ home.nix
@@ -17,6 +17,7 @@
     gh
     waybar
     libreoffice
+    wl-clipboard
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
@@ -38,7 +39,163 @@
     EDITOR = "nvim";
   };
 
-  programs.hyprland.enable = true;
+  wayland.windowManager.hyprland = {
+    enable = true;
+    settings = {
+      monitor = [
+        "eDP-1,1920x1080@60,0x0,1"
+	"HDMI-A-1,1920x1080@144,0x-1080,1"
+	",preferred,auto,1"
+      ];
+
+      "$terminal" = "kitty";
+      "$fileManager" = "nemo";
+      "$menu" = "rofi -show drun";
+
+      env = [
+        "XCURSOR_SIZE,24"
+	"QT_QPA_PLATFORMTHEME,qt5ct"
+      ];
+
+      input = {
+        kb_layout = "us";
+	kb_variant = "";
+	kb_model = "";
+	kb_options = "";
+	kb_rules = "";
+
+	follow_mouse = 1;
+
+	touchpad = {
+	  natural_scroll = "yes";
+	  disable_while_typing = false;
+	  scroll_factor = 0.5;
+	};
+
+	sensitivity = 0.2;
+      };
+
+      general = {
+        gaps_in = 10;
+	gaps_out = 10;
+	border_size = 2;
+	"col.active_border" = "rgba(cba6f7ee) rgba(f38ba8ee) 45deg";
+	"col.inactive_border" = "rgba(595959aa)";
+
+	layout = "dwindle";
+
+	allow_tearing = false;
+      };
+
+      decoration = {
+        rounding = 5;
+
+	blur = {
+	  enabled = true;
+	  size = 3;
+	  passes = 1;
+	};
+
+	drop_shadow = "yes";
+	shadow_range = 4;
+	shadow_render_power = 3;
+	"col.shadow" = "rgba(1a1a1aee)";
+      };
+
+      animations = {
+        enabled = "yes";
+
+	bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
+
+	animation = [
+	  "windows, 1 ,7, myBezier"
+	  "windowsOut, 1, 7, default, popin 80%"
+	  "border, 1, 10, default"
+	  "borderangle, 1, 8, default"
+	  "fade, 1, 7, default"
+	  "workspaces, 1, 6, default"
+	];
+      };
+
+      dwindle = {
+        pseudotile = "yes";
+	preserve_split = "yes";
+      };
+
+      master = {
+        new_is_master = true;
+      };
+
+      gestures = {
+        workspace_swipe = "on";
+      };
+
+      misc = {
+        force_default_wallpaper = "-1";
+      };
+
+      "$mainMod" = "SUPER";
+
+      bind = [
+        "$mainMod_SHIFT, Q, killactive"
+	"$mainMod_SHIFT, P, exit,"
+	"$mainMod, E, exec, $fileManager"
+	"$mainMod, V, togglefloating"
+	"$mainMod, D, exec, $menu"
+	"$mainMod, F, fullscreen, 0"
+	"$mainMod, Return, exec, $terminal"
+	"$mainMod, Z, exec, firefox"
+	"$mainMod_SHIFT, Z, exec, firefox --private-window"
+	
+	"$mainMod, left, movefocus, l"
+	"$mainMod, right, movefocus, r"
+	"$mainMod, up, movefocus, u"
+	"$mainMod, down, movefocus, d"
+
+	"$mainMod SHIFT, left, movewindow, l"
+	"$mainMod SHIFT, right, movewindow, r"
+	"$mainMod SHIFT, up, movewindow, u"
+	"$mainMod SHIFT, down, movewindow, d"
+
+	"$mainMod, 1, workspace, 1"
+	"$mainMod, 2, workspace, 2"
+	"$mainMod, 3, workspace, 3"
+	"$mainMod, 4, workspace, 4"
+	"$mainMod, 5, workspace, 5"
+	"$mainMod, 6, workspace, 6"
+	"$mainMod, 7, workspace, 7"
+	"$mainMod, 8, workspace, 8"
+	"$mainMod, 9, workspace, 9"
+	"$mainMod, 0, workspace, 0"
+
+	"$mainMod SHIFT, 1, movetoworkspace, 1"
+	"$mainMod SHIFT, 2, movetoworkspace, 2"
+	"$mainMod SHIFT, 3, movetoworkspace, 3"
+	"$mainMod SHIFT, 4, movetoworkspace, 4"
+	"$mainMod SHIFT, 5, movetoworkspace, 5"
+	"$mainMod SHIFT, 6, movetoworkspace, 6"
+	"$mainMod SHIFT, 7, movetoworkspace, 7"
+	"$mainMod SHIFT, 8, movetoworkspace, 8"
+	"$mainMod SHIFT, 9, movetoworkspace, 9"
+	"$mainMod SHIFT, 0, movetoworkspace, 0"
+
+	"$mainMod, X, swapactiveworkspaces, eDP-1 HDMI-A-1"
+
+	"$mainMod, S, togglespecialworkspace, magic"
+	"$mainMod SHIFT, S, movetoworkspace, special:magic"
+      ];
+
+      bindm = [
+	"$mainMod, mouse:272, movewindow"
+	"$mainMod, mouse:273, resizewindow"
+      ];
+
+      exec-once = [
+        "waybar"
+	"hyprpaper"
+      ];
+    };
+  };
 
   programs.zsh = {
     enable = true;
