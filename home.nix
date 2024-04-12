13e13c7e7b49335f74441ac5d892b08f88f7b1fc
Jacob Busby <98309644+Jacob-P-Busby@users.noreply.github.com>
2024-04-12 18:52:16 +0800

    changed workspace 0 to 10

--- home.nix
+++ home.nix
@@ -172,7 +172,7 @@
 	"$mainMod, 7, workspace, 7"
 	"$mainMod, 8, workspace, 8"
 	"$mainMod, 9, workspace, 9"
-	"$mainMod, 0, workspace, 0"
+	"$mainMod, 0, workspace, 10"
 
 	"$mainMod SHIFT, 1, movetoworkspace, 1"
 	"$mainMod SHIFT, 2, movetoworkspace, 2"
@@ -183,7 +183,7 @@
 	"$mainMod SHIFT, 7, movetoworkspace, 7"
 	"$mainMod SHIFT, 8, movetoworkspace, 8"
 	"$mainMod SHIFT, 9, movetoworkspace, 9"
-	"$mainMod SHIFT, 0, movetoworkspace, 0"
+	"$mainMod SHIFT, 0, movetoworkspace, 10"
 
 	"$mainMod, X, swapactiveworkspaces, eDP-1 HDMI-A-1" # Switch between laptop and hdmi monitor
 
