82eef0c8676e1d9ccccee2fbb5868153053ba0de
Jacob Busby <98309644+Jacob-P-Busby@users.noreply.github.com>
2024-05-04 15:27:11 +0800

    Added some lsp binds

--- home/nixvim/lsp.nix
+++ home/nixvim/lsp.nix
@@ -13,5 +13,20 @@
         installRustc = false;
       };
     };
+
+    keymaps = {
+      diagnostic = {
+        "<leader>j" = "goto_next";
+        "<leader>k" = "goto_prev";
+      };
+
+      lspBuf = {
+        K = "hover";
+        gD = "references";
+        gd = "definition";
+        gi = "implementation";
+        gt = "type_definition";
+      };
+    };
   };
 }
