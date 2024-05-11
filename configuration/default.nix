55a472074bd822394cee047fb5bee4f671b7b055
Jacob Busby <98309644+Jacob-P-Busby@users.noreply.github.com>
2024-05-11 14:19:54 +0800

    Added tlp

--- configuration/default.nix
+++ configuration/default.nix
@@ -33,6 +33,22 @@
     size = 8 * 1024;
   }];
 
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performace";
+
+      CPU_MIN_PERF_ON_AC = 0;
+      CPU_MAX_PERF_ON_AC = 100;
+      CPU_MIN_PERF_ON_BAT = 0;
+      CPU_MAX_PERF_ON_BAT = 40;
+    };
+  };
+
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
