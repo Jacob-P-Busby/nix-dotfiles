cb74a4317a614758150a25eeff5d70b20ccf04cc
Jacob Busby <98309644+Jacob-P-Busby@users.noreply.github.com>
2024-06-28 08:35:28 +0800

    Added rua alias

--- home/zsh/default.nix
+++ home/zsh/default.nix
@@ -9,37 +9,37 @@
 
     shellAliases = {
       off = "shutdown now";
+      rua = "rfkill unblock all";
     };
 
-    initExtra = 
-      ''
-        function lk {
-          cd "$(walk --icons "$@")"
-        }
+    initExtra = ''
+      function lk {
+        cd "$(walk --icons "$@")"
+      }
 
-        function update {
-          if git diff-index --cached HEAD --exit-code >& - 2>& -
-          then
-            home-manager switch --flake ~/.dotfiles
-          else
-            echo "Git tree is dirty"
-          fi
-        }
+      function update {
+        if git diff-index --cached HEAD --exit-code >& - 2>& -
+        then
+          home-manager switch --flake ~/.dotfiles
+        else
+          echo "Git tree is dirty"
+        fi
+      }
 
-        function update-full {
-          cd ~/.dotfiles
-          if git diff-index --cached HEAD --exit-code >& - 2>& -
-          then
-            nix flake update
-            git add flake.lock
-            git commit -m 'flake.lock bump'
-            home-manager switch --flake ~/.dotfiles
-          else
-            echo "Git tree is dirty"
-          fi
-          cd - >& -
-        }
-      '';
+      function update-full {
+        cd ~/.dotfiles
+        if git diff-index --cached HEAD --exit-code >& - 2>& -
+        then
+          nix flake update
+          git add flake.lock
+          git commit -m 'flake.lock bump'
+          home-manager switch --flake ~/.dotfiles
+        else
+          echo "Git tree is dirty"
+        fi
+        cd - >& -
+      }
+    '';
 
     oh-my-zsh = {
       enable = true;
