b9194c3a9462c69d0b6f91c95c3e94ba315687a7
Jacob Busby <98309644+Jacob-P-Busby@users.noreply.github.com>
2024-08-16 10:00:12 +0800

    Added helix language settings for tailwindcss

--- home/helix/default.nix
+++ home/helix/default.nix
@@ -24,7 +24,39 @@
           render = true;
         };
       };
+    };
 
+    languages = {
+      language = [
+        {
+          name = "html";
+          language-servers = [ 
+            "vscode-html-language-server"
+            "tailwindcss-ls"
+          ];
+        }
+        {
+          name = "css";
+          language-servers = [
+            "vscode-css-language-server"
+            "tailwindcss-ls"
+          ];
+        }
+        {
+          name = "jsx";
+          language-servers = [
+            "typescript-language-server"
+            "tailwindcss-ls"
+          ];
+        }
+        {
+          name = "tsx";
+          language-servers = [
+            "typescript-language-server"
+            "tailwindcss-ls"
+          ];
+        }
+      ];
     };
   };
 }
